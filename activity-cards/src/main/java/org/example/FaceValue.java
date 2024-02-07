b083d6374e516717304fb15d52bde9e8f5ec298f
jpherrmann3 <james_herrmann3@outlook.com>
2024-02-06 19:20:09 -0700

    Changed to enum

--- activity-cards/src/main/java/org/example/FaceValue.java
+++ activity-cards/src/main/java/org/example/FaceValue.java
@@ -1,5 +1,5 @@
 package org.example;
 
-public interface FaceValue {
+public enum FaceValue {
 
 }
--- activity-cards/src/main/java/org/example/PinochleFaceValue.java
+++ activity-cards/src/main/java/org/example/PinochleFaceValue.java
@@ -1,5 +1,5 @@
 package org.example;
 
-public class PinochleFaceValue implements FaceValue{
+public class PinochleFaceValue {
 
 }
