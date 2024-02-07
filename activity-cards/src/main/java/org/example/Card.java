9836e88207cf86dd2d10cc0628d15bcd90b373a0
jpherrmann3 <james_herrmann3@outlook.com>
2024-02-06 19:32:21 -0700

    Adding default setters and getters

--- activity-cards/src/main/java/org/example/Card.java
+++ activity-cards/src/main/java/org/example/Card.java
@@ -1,5 +1,41 @@
 package org.example;
+import java.util.Objects;
 
-public class Card {
+public class Card implements Comparable<Card>{
+
+    private Suit suit;
+    private FaceValue faceValue;
+
+    private Card() {
+    }
+
+    public Card(Suit suit, FaceValue faceValue) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public FaceValue getFaceValue() {
+        return this.faceValue;
+    }
+
+    public void setFaceValue(FaceValue faceValue) {
+        this.faceValue = faceValue;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " suit='" + getSuit() + "'" +
+            ", value='" + getFaceValue() + "'" +
+            "}";
+    }
 
 }
